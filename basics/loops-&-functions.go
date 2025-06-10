36f7d0a6aee58d170f82d8922760fd0dd0f6e9bd
Justine Hornales <jchornales.dev@gmail.com>
2025-06-10 21:44:10 +0800

    Basic Calculator

--- basics/loops-&-functions.go
+++ basics/loops-&-functions.go
@@ -14,7 +14,48 @@ func Power(number float64, power int ) float64 {
 
 }
 
+func Add(a,b float64) float64{
+	return a+b
+}
+
+
+func Subtract(a,b float64) float64{
+	return a-b
+}
+
+
+
+func Multiply(a,b float64) float64{
+	return a*b
+}
+
+
+
+func Divide(a,b float64) float64{
+	return a/b
+}
+
+
+func Calculate(a float64,b float64, operation string)  {
+	var result float64
+
+	switch operation {
+	case "+":
+		result= Add(a,b)
+	case "-":
+		result= Subtract(a,b)
+	case "*":
+		result= Multiply(a,b)
+	case "/":
+		result=Divide(a,b)
+	default:
+		fmt.Println("Please select operator")
+	}
+	// No changes needed here for rune literals.
+	fmt.Println("Result", result)
+}
+
 
 func main(){
-	fmt.Println("Result: ", Power(4, 2))
+	Calculate(5,2, "/" )
 }
